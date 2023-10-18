7438a66d59e521d52373d6a99d2fa3321cf25f14
shirokovnv <shirokovnv@gmail.com>
2023-10-18 16:02:12 +0500

    Add search form object

---
+++ src/Form/WbProductsSearchForm.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Cake\Form\Form;
+use Cake\Form\Schema;
+use Cake\Validation\Validator;
+
+/**
+ * WbProductsSearch Form.
+ */
+class WbProductsSearchForm extends Form
+{
+    private const KEY_QUERY = 'query';
+
+    /**
+     * Builds the schema for the modelless form
+     *
+     * @param \Cake\Form\Schema $schema From schema
+     * @return \Cake\Form\Schema
+     */
+    protected function _buildSchema(Schema $schema): Schema
+    {
+        return $schema->addField(self::KEY_QUERY, 'string');
+    }
+
+    /**
+     * Form validation builder
+     *
+     * @param \Cake\Validation\Validator $validator to use against the form
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        return $validator
+            ->notEmptyString(self::KEY_QUERY)
+            ->maxLength(self::KEY_QUERY, 255);
+    }
+
+    /**
+     * Defines what to execute once the Form is processed
+     *
+     * @param array $data Form data.
+     * @return bool
+     */
+    protected function _execute(array $data): bool
+    {
+        return true;
+    }
+}
