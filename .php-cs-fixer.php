b5de336128d761db390ac6ed7e95694f5288a820
shirokovnv <shirokovnv@gmail.com>
2023-10-19 18:07:55 +0500

    Add import rules for php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,7 +4,12 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $rules = [
-    '@PSR12' => true
+    '@PSR12' => true,
+    'no_unused_imports' => true, // NEW
+    'ordered_imports' => [
+        'sort_algorithm' => 'alpha',
+        'imports_order' => ['const', 'class', 'function'], // NEW https://mlocati.github.io/php-cs-fixer-configurator/#version:3.3%7Cfixer:ordered_imports
+    ],
 ];
 
 $finder = Finder::create()
