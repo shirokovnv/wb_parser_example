a7e9d1af53c528b8bf7f751a5d0d6dfc780c7b12
shirokovnv <shirokovnv@gmail.com>
2023-10-19 12:40:12 +0500

    Use better semantics for converter impl

--- src/Service/WbProducts/Converter/WbSearchProductsConverter.php
+++ src/Service/WbProducts/Converter/WbSearchProductsConverter.php
@@ -8,9 +8,9 @@ use App\Service\WbProducts\Converter\Exception\ConvertException;
 use App\Service\WbProducts\DTO\WbProductEntity;
 
 /**
- * Зона ответственности: преобразование сырых (строковый JSON) данных товаров в массив DTO WbProductEntity;
+ * Зона ответственности: преобразование сырых (строковый JSON c search.wb.ru) данных товаров в массив DTO WbProductEntity;
  */
-class WbProductsJsonConverter implements WbProductsConverterInterface
+class WbSearchProductsConverter implements WbProductsConverterInterface
 {
     private const KEY_DATA = 'data';
 
